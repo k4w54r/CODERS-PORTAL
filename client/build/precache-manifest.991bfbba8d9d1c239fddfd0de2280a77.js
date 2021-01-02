44258d49e1aa048169cdd576247ae1145fd59c15
Kawsar <k4w54r13@gmail.com>
2021-01-03 02:28:16 +0600

    Ready for deployment

---
+++ client/build/precache-manifest.991bfbba8d9d1c239fddfd0de2280a77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe0213a20446fa4de319e224a67ac92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cb9260ce2c5e6bfe3ad",
+    "url": "/static/css/main.4a69b41d.chunk.css"
+  },
+  {
+    "revision": "85f7a13a51d6866fe8e6",
+    "url": "/static/js/2.5226324f.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.5226324f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb9260ce2c5e6bfe3ad",
+    "url": "/static/js/main.93b268ff.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
